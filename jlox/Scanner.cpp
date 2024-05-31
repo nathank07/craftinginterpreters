ceea06741c74cfeef69029fe31717e2facc87d02
nathank07 <nathank7256@gmail.com>
2024-05-30 20:59:10 -0500

    Fixed string support

--- jlox/Scanner.cpp
+++ jlox/Scanner.cpp
@@ -39,7 +39,7 @@ void Scanner::scanToken() {
         case '\n':
             line++;
             break;
-        case '"': break;
+        case '"': string(); break;
         default:
             if(isdigit(c)) {
                 number();
